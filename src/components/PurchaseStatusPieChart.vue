d01ff5e53ebf0e6196c34503991adabba37688cb
David Kassa <david.kassa@gmail.com>
2018-04-29 01:45:17 -0500

    Basic working chart from local json data

--- src/components/PurchaseStatusPieChart.vue
+++ src/components/PurchaseStatusPieChart.vue
@@ -1,33 +1,33 @@
 <script>
-import { Pie } from 'vue-chartjs';
+import { Pie, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins;
 
 export default {
   extends: Pie,
-  props: ['waiting', 'passed', 'taken'],
-    data () {
+  mixins: [reactiveProp],
+  props: ["waiting", "passed", "taken"],
+  mounted() {
+    this.renderChart(this.chartData, {
+      responsive: true,
+      maintainAspectRatio: false
+    }); //,options);
+  },
+  computed: {
+    chartData: function() {
       return {
-        datacollection: null
-      }
-    },
-    mounted () {
-      this.fillData();
-      this.renderChart(this.datacollection);//,options);
-    },
-    methods: {
-      fillData () {
-        this.datacollection = {
-          labels: ['Waiting', 'Passed', 'Taken'],
-          datasets: [
-            {
-              backgroundColor: ['#F8B379','#61C661','#f87979'],
-              data: [this.waiting, this.passed, this.taken]
-            }
-          ]
-        }
-      },
-      getRandomInt () {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-      }      
+        labels: ["Waiting", "Passed", "Taken"],
+        datasets: [
+          {
+            backgroundColor: ["#F8B379", "#61C661", "#f87979"],
+            data: [this.waiting, this.passed, this.taken]
+          }
+        ]
+      };
     }
-}
+    // },
+    // getRandomInt() {
+    //   return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    // }
+  }
+};
 </script>
--- src/views/Data.vue
+++ src/views/Data.vue
@@ -1,20 +1,44 @@
 <template>
   <div class="data">
     <h1>This is the data page</h1>
-    <purchase-status-pie-chart :waiting=10 :passed=6 :taken=4 />
+    <purchase-status-pie-chart :waiting=waiting :passed=passed :taken=taken class="chart" />
   </div>
 </template>
 
 <script>
-import PurchaseStatusPieChart from '@/components/PurchaseStatusPieChart.vue'; // @ is an alias to /src
+import PurchaseStatusPieChart from "@/components/PurchaseStatusPieChart.vue"; // @ is an alias to /src
+import jsonData from "@/../data/4.2018.json";
 
 export default {
   components: {
     PurchaseStatusPieChart
+  },
+  data() {
+    return {
+      waiting: this.waiting,
+      passed: this.passed,
+      taken: this.taken
+    };
+  },
+  mounted() {
+    this.loadJson();
+  },
+  methods: {
+    loadJson() {
+      // console.log(jsonData);
+      this.waiting = jsonData.find(a => a.name == "Waiting").count;
+      this.passed = jsonData.find(a => a.name == "Passed").count;
+      this.taken = jsonData.find(a => a.name == "Taken").count;
+    }
   }
 };
 </script>
 
+<style scoped lang="scss">
+.chart {
+  max-width: 400px;
+}
+</style>
 
 
 // {
