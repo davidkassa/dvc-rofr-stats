84053a4073557ca56d2cd148ebbe99608e3a73ca
David Kassa <david.kassa@gmail.com>
2018-05-05 08:09:20 -0500

    add gitlab ci support

--- src/main.js
+++ src/main.js
@@ -18,15 +18,19 @@ Vue.config.productionTip = false;
 Vue.use(Buefy);
 Vue.use(VueFire);
 
+/* eslint-disable no-undef */
+
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyD0795zezCZcML2tL5jXGe08IpQx26eIkM",
-  authDomain: "dvc-rofr-stats-dev.firebaseapp.com",
-  databaseURL: "https://dvc-rofr-stats-dev.firebaseio.com",
-  projectId: "dvc-rofr-stats-dev",
-  storageBucket: "dvc-rofr-stats-dev.appspot.com",
-  messagingSenderId: "365161053410"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
 };
+/* eslint-enable no-undef */
+
 firebase.initializeApp(config);
 export const db = firebase.firestore();
 
