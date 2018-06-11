616adfdcf099452a9e09329995f754635061ad77
David Kassa <david.kassa@gmail.com>
2018-06-10 23:59:38 -0500

    setup disboard parsing functions

--- src/components/FeedbackDialog.vue
+++ src/components/FeedbackDialog.vue
@@ -9,7 +9,7 @@ export default {
       this.$modal.open({
         parent: this,
         content:
-          '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQCt28Gxr5VfS_bi0vjN1FSRk2Q4XLOXkKU7vWOasFF3bE3Q/viewform?embedded=true" width="100%" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
+          '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfQCt28Gxr5VfS_bi0vjN1FSRk2Q4XLOXkKU7vWOasFF3bE3Q/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
       });
     }
   },
@@ -22,5 +22,13 @@ export default {
 <style lang="scss" >
 .modal-card-body {
   padding: 0;
+}
+// .modal,
+// .modal-content {
+//   --webkit-overflow-scrolling: touch;
+//   overflow-y: scroll;
+// }
+.modal-content div {
+  height: 1200px;
 }
 </style>
