4021222362bd75612f668c2ce044e33a5a018d3b
David Kassa <david.kassa@gmail.com>
2019-09-08 09:45:27 -0500

    update lint deps

--- src/views/StatsRoot.vue
+++ src/views/StatsRoot.vue
@@ -26,7 +26,7 @@
             v-show="Object.keys(meta).length !== 0"
             :href="meta.url"
             target="_blank"
-          >{{ meta.text }} Last Updated: {{ meta.epoch | moment }}</a>
+            >{{ meta.text }} Last Updated: {{ meta.epoch | moment }}</a>
         </div>
       </div>
       <rofr-data-table :selected.sync="selected" :data="contractData" />
