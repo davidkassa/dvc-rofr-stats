f489ce488085ff47e34512226dff13cfbb773385
David Kassa <david.kassa@gmail.com>
2020-07-27 13:09:31 -0500

    update eslint to v7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,44 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier/recommended"],
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+
   parserOptions: {
     parser: "babel-eslint",
   },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  extends: [
+    "plugin:vue/essential",
+    "@vue/prettier/recommended",
+    "eslint:recommended",
+    "@vue/prettier",
+  ],
 };
